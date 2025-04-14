db95698624af45f16deeaf7e54a84107e82ba1d4
mthuvu <helloitsthu@gmail.com>
2025-04-13 17:19:58 -0700

    Added crud and jwt

---
+++ backend/src/backend/auth.py
@@ -0,0 +1,27 @@
+from src.backend.config import settings
+from fastapi.security import OAuth2PasswordBearer
+from pathlib.context import CryptContext
+from datetime import datetime, timedelta, UTC
+import jwt
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token") # replace "token" with api endpoint for the token
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash (password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + {
+        expires_delta if expires_delta else timedelta(minutes=15)
+    }
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+#Add decode access token function
---
+++ backend/src/backend/services/user.py
@@ -0,0 +1,30 @@
+from src.backend.config import get_settings
+from src.backend.schemas.user import UserCreate
+from sqlalchemy.orm import Session
+from src.backend.auth import get_password_hash
+from src.backend.models.user import User
+
+settings = get_settings()
+
+# User CRUD operations
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    verification_code = "1234"  # Replace with actual verification code logic
+    db_user = User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        password=hashed_password,
+        verification_code=verification_code,
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# in user.py in routes folder, add the following code
+# import app.services.user as user_service
+# replace the return message in create_user with the following code
+#   new_user =  userservice.create_user()
+#   return new_user
