62058597e4b75119694cc1027e7303ef4697a6c5
Ray Kim <85251403+whyneedtoknow@users.noreply.github.com>
2025-04-13 22:24:48 -0700

    BadMergeuser.py

--- backend/src/backend/schemas/user.py
+++ backend/src/backend/schemas/user.py
@@ -1,14 +1,32 @@
-from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
-class UserBase(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-class UserCreate(UserBase):
-    password: str
-class User(UserBase):
+from pydantic import BaseModel, field_validator
+
+
+class HabitBase(BaseModel):
+    month: int = datetime.now(datetime.timezone.UTC).month
+    day: int = datetime.now(datetime.timezone.UTC).day
+    year: int = datetime.now(datetime.timezone.UTC).year
+    name: str | None = None]
+    duration: timedelta = timedelta(0)
+    @field_validator('duration', mode='before')
+    @classmethod
+    def parse_duration(cls, value):
+        if isinstance(value, int):
+            return timedelta(seconds=value)
+        if isinstance(value, str):
+            pass
+        return value
+    quantity: int = 0
+    category: str
+    description: str | None = None
+    completed: bool = False
+class HabitCreate(HabitBase):
+    pass
+class Habit(HabitBase):
     id: int
-    acc_created = datetime
     class Config:
         orm_mode = True
+        json_encoders = {
+          timedelta: lambda v: int(v.total_seconds())
+        }
