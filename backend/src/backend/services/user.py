3894e78da07fe3d8d11808c84d7164c95a215692
mthuvu <helloitsthu@gmail.com>
2025-04-13 20:06:01 -0700

    create habit, users, event

--- backend/src/backend/services/user.py
+++ backend/src/backend/services/user.py
@@ -23,6 +23,41 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(db_user)
     return db_user
 
+def create_habit(db: Session, habit: HabitCreate, user_id: int):
+    db_habit = Habit(
+        user_id=user_id,
+        month=habit.month,
+        day=habit.day,
+        year=habit.year,
+        name=habit.name,
+        duration=habit.duration,
+        quantity=habit.quantity,
+        category=habit.category,
+        description=habit.description,
+        completed=habit.completed,
+    )
+    db.add(db_habit)
+    db.commit()
+    db.refresh(db_habit)
+    return db_habit
+
+def create_event(db: Session, event: EventCreate, user_id: int):
+    db_event = Event(
+        user_id=user_id,
+        name=event.name,
+        start_date=event.start_date,
+        end_date=event.end_date,
+        start_time=event.start_time,
+        end_time=event.end_time,
+        recurring=event.recurring,
+        description=event.description,
+    )
+    db.add(db_event)
+    db.commit()
+    db.refresh(db_event)
+    return db_event
+
+
 # in user.py in routes folder, add the following code
 # import app.services.user as user_service
 # replace the return message in create_user with the following code
