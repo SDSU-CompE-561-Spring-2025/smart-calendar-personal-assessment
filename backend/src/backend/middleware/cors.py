ce824ec1362ec752134791db499f36ece13f36a5
JehnFlores <johnadrielflores@gmail.com>
2025-04-13 21:00:09 -0700

    Completed and Tested Middleware

---
+++ backend/src/backend/middleware/cors.py
@@ -0,0 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+def get_cors_config():
+    return {
+        "middleware_class": CORSMiddleware,
+        "options": {
+            "allow_origins": ["*"],
+            "allow_credentials": True,
+            "allow_methods": ["*"],
+            "allow_headers": ["*"]
+        }
+    }
