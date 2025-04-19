00597a4b073c3937ebbabac9b9d9170a1c7b6fdc
John Flores <114787686+JehnFlores@users.noreply.github.com>
2025-04-19 12:30:36 -0700

    Revert "Update calendar.py"

--- backend/src/backend/routes/calendar.py
+++ backend/src/backend/routes/calendar.py
@@ -1,35 +1,24 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from typing import List
+from fastapi import APIRouter
 
-from backend.schemas.event import Event, EventCreate, create_event, update_event, delete_event
-from backend.routes.calendar import calendar
-from backend.schemas.calendar import Calendar, CalendarCreate, create_calendar
-from backend.services.dependencies import get_db
+router = APIRouter()
 
-router = APIRouter(prefix="/api/events", tags=["Events"])
-router = APIRouter(prefix="/api/calendars", tags=["Calendars"])
+#Calendar
+@router.get("/events")
+def retrieve_all_events():
+    return {"message": "All Events Retrieved"}
 
-@router.post("/", response_model=Calendar)
-def create_calendar_endpoint(calendar: CalendarCreate, user_id: int, db: Session = Depends(get_db)):
-    return create_calendar(db=db, calendar=calendar, user_id=user_id)
+@router.get("/events/{eventId}")
+def retrieve_event():
+  return {"message": "Event Retrieved"}
 
-@router.post("/", response_model=dict)
-def create_event_endpoint(event: EventCreate, user_id: int, db: Session = Depends(get_db)):
-    return create_event(db=db, event=event, user_id=user_id)
+@router.post("/events")
+def create_event():
+    return {"message": "Event Created"}
 
-@router.put("/{event_id}", response_model=Event)
-def update_event_endpoint(event_id: int, updated_event: EventCreate, db: Session = Depends(get_db)):
-    return update_event(db=db, event_id=event_id, updated_event=updated_event)
+@router.put("/events/{eventId}")
+def update_event():
+    return {"message": "Event Updated"}
 
-@router.get("/{event_id}", response_model=Event)
-def retrieve_event(event_id: int, db: Session = Depends(get_db)):
-    return retrieve_event(db=db, event_id=event_id)
-
-@router.get("/", response_model=list[Event])
-def retrieve_events(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-    return retrieve_events(db=db, skip=skip, limit=limit)
-
-@router.delete("/{event_id}", response_model=dict)
-def delete_event_endpoint(event_id: int, db: Session = Depends(get_db)):
-    return delete_event(db=db, event_id=event_id)
+@router.delete("/events/{eventId}")
+def delete_event():
+    return {"message": "Event Deleted"}
