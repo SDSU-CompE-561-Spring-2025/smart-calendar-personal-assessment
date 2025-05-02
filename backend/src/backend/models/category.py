ce0cb1dd819334921cf748370727f02bf4d8ff84
JehnFlores <johnadrielflores@gmail.com>
2025-05-02 12:18:07 -0700

    Added Uniqueness to category names/cleaned up code

--- backend/src/backend/models/category.py
+++ backend/src/backend/models/category.py
@@ -6,7 +6,7 @@ class Category(Base):
     __tablename__ = 'categories'
 
     id = Column(Integer, primary_key = True, autoincrement=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, unique = True, index = True, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))  # Link to user
 
     user = relationship("User", back_populates="categories")
