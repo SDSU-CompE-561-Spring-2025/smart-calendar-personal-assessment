a69f8e76f8dc6762cc3eac3bfafdde4860782ca6
whyneedtoknow <rayskim221@gmail.com>
2025-05-06 19:08:44 -0700

    Sidebar contains user info now

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import * as React from "react"
-import { NavUser } from "./nav-user"
+import React from "react"
 import { useRouter } from "next/navigation"
 import DragAndDropList from "@/components/habits"
-import { useState }  from "react" 
-import { API_HOST_BASE_URL } from "@/lib/constants" 
-
+import { NavUser } from "./nav-user"
 import {
   Sidebar,
   SidebarContent,
@@ -17,34 +14,13 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 
-const data = {
-  user: { // get user stuff from backend
-    name: "User",
-    email: "email@example.com",
-    avatar: "/potthing.jpg", // figure this out later
-  },
-}
-
-export function AppSidebar({ ...props }: 
-  React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const router = useRouter()
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [email, setEmail] = useState("")
-  const [error, setError] = useState<string | null>(null)
-  const [loading, setLoading] = useState(false)
-  
-
-  
-  const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    router.push("/signin");
-  }
 
   return (
     <Sidebar {...props}>
       <SidebarHeader className="h-15 border-b border-sidebar-border">
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
